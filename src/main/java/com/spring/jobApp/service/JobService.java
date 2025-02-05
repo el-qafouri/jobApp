f8c0bc7dd470395382afa4a6332abcee0a7d0cff
Elham Qafouri <elham.qafouri.91@gmail.com>
2025-02-05 13:22:43 +0330

    fix controller methods

---
+++ src/main/java/com/spring/jobApp/service/JobService.java
@@ -0,0 +1,23 @@
+package com.spring.jobApp.service;
+
+import com.spring.jobApp.model.JobPost;
+import com.spring.jobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs() {
+        return repo.getAllJobs();
+    }
+}
